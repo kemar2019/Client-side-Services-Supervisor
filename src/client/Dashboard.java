c511f9bce84904cd56e5b9171dfefa35a78db6ad
kemar2019 <50750275+kemar2019@users.noreply.github.com>
2023-04-21 03:52:13 -0500

    changes to dashboard

--- src/client/Dashboard.java
+++ src/client/Dashboard.java
@@ -33,8 +33,9 @@ public class Dashboard extends JFrame {
 
 	/**
 	 * Create the frame.
+	 * @param client 
 	 */
-	public Dashboard() {
+	public Dashboard(ClientHandler client) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1209, 679);
 		getContentPane().setLayout(null);
